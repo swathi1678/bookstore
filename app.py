4932faef059e83596abf053dcee4c9c3d6e64686
Swathi <swathi@Swathis-MacBook-Air.local>
2025-06-13 20:47:07 +0530

    fix: removed deprecated before_first_request for Flask 3

--- app.py
+++ app.py
@@ -7,23 +7,21 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:/
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
 @app.route('/')
 def index():
     books = Book.query.all()
     return render_template('index.html', books=books)
 
-@app.route('/search', methods=['GET'])
+@app.route('/search')
 def search():
-    query = request.args.get('query')
+    query = request.args.get('query', '')
     books = Book.query.filter(
         (Book.title.ilike(f"%{query}%")) | (Book.author.ilike(f"%{query}%"))
     ).all()
     return render_template('search.html', books=books, query=query)
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
